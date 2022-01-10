ac2e2a22f703905bd440ff1e1dc78e1580614e67
Arka Mukherjee <85149698+Rendro1@users.noreply.github.com>
2022-01-10 11:04:34 +0530

    lec_45

--- backend/index.js
+++ backend/index.js
@@ -2,9 +2,15 @@ const connectToMongo = require('./db');
 connectToMongo();
 
 
+
 const express = require('express')
 const app = express()
 const port = 3000
+
+app.use(express.json()) // ata use hoi jate request marle undefine na dekhai body ar content ta dekhay
+// Available Routes->
+app.use('/api/auth', require('./routes/auth'))
+app.use('/api/notes', require('./routes/notes'))
 
 app.get('/', (req, res) => {
   res.send('Hello Arka VAi let learn mongoDb happily!')
